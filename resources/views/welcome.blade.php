17c8f32202c7d4aba4b15af3a5e1f20bd80fb35e
jwsvengit7 <chiorlujack@gmail.com>
2024-10-25 20:45:21 +0100

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link href="{{ mix('build/assets/app.css') }}" rel="stylesheet">
-    <script src="{{ mix('build/assets/app.js') }}" defer></script>
-
-
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
