0dea2da82c7b8b38f1ba7d5fa4bd6c0f9d0b4eba
jwsvengit7 <chiorlujack@gmail.com>
2024-10-25 16:23:26 +0100

    build

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <script src="{{ asset('build/assets/app.js') }}"></script>
-    <link href="{{ asset('build/assets/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('build/assets/app-B2l35M62.js') }}"></script>
+    <link href="{{ asset('build/assets/app-D5YwssPJ.css') }}" rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
