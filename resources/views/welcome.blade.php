5b11f80454cbcb88961f355a5d9457ea23175c63
jwsvengit7 <chiorlujack@gmail.com>
2024-10-29 15:39:41 +0100

    inti

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 
-
-
 </head>
 <body>
 
