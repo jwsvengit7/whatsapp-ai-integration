394ae020d096102445d3f65bb33944e105f41484
jwsvengit7 <chiorlujack@gmail.com>
2024-11-07 20:59:54 +0100

    e

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        Log::info("Logger");
         $schedule->command('messages:send-scheduled')->everyMinute();
     }
 
